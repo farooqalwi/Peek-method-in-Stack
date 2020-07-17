2bf2d3860127f0da75145452299a4d7d50b6383f
Farooq Alwi <farooqalwi313@gmail.com>
2020-07-17 12:54:35 +0500

    Peek method done
    Peek method retrun the element without removing it

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peek_method_in_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+            stack.Push(40);
+
+            stack.Print();
+
+            Console.WriteLine("After Peek");
+
+            object num = stack.Peek();
+            stack.Print();
+
+            Console.Write("Peek value: ");
+            Console.WriteLine(num);
+
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peek_method_in_Stack
+{
+    class Stack
+    {
+        ArrayList stack;
+
+        public Stack()
+        {
+            stack = new ArrayList();
+        }
+
+        public void Push(object element)
+        {
+            stack.Add(element);
+        }
+
+        public object Pop()
+        {
+            object element = stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count - 1);
+            return element;
+        }
+
+        public object Peek()
+        {
+            object element = stack[stack.Count - 1];
+            return element;
+        }
+
+        public void Print()
+        {
+            for (int i = stack.Count - 1; i >= 0; i--)
+            {
+                Console.WriteLine(stack[i]);
+            }
+        }
+    }
+}
